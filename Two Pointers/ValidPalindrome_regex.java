799628935fd47922e08cbc74296e0066fa0c1522
Bheb Developer <bhebdeveloper@gmail.com>
2024-07-12 01:29:00 +0530

    12/07/2024

--- Two Pointers/ValidPalindrome_regex.java
+++ Two Pointers/ValidPalindrome_regex.java
@@ -1,24 +1,22 @@
-public class ValidPalindromeString {
+public class ValidPalindrome_regex {
     public static void main(String[] args) {
         String str = "A man, a plan, a canal: Panama";
         System.out.println(isPalindrome(str));
     }
 
     static Boolean isPalindrome(String str) {
+        str = str.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
 
-        String finalString = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        int size = finalString.length();
+        int size = str.length();
 
-        if (size == 0 || size == 1) {
+        if (size == 0 || size == 1)
             return true;
-        }
 
         for (int i = 0; i < size / 2; i++) {
-            if (finalString.charAt(i) != finalString.charAt(size - 1 - i)) {
+            if (str.charAt(i) != str.charAt(size - 1 - i)) {
                 return false;
             }
         }
-
         return true;
     }
 }
