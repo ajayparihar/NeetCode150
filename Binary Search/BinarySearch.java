adaf0ebee393de44f3a2461852bd0ac5bdf6c542
Bheb Developer <bhebdeveloper@gmail.com>
2024-07-17 02:50:06 +0530

    17/07/2024

--- Binary Search/BinarySearch.java
+++ Binary Search/BinarySearch.java
@@ -12,13 +12,12 @@ public class BinarySearch {
 
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
+            if (nums[mid] == target)
                 return mid;
-            } else if (nums[mid] < target) {
+            else if (nums[mid] < target)
                 left = mid + 1;
-            } else {
+            else
                 right = mid - 1;
-            }
         }
         return -1;
     }
