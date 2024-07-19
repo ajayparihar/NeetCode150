9e59764a8debb7ba10bb9c64a7aea69eecfdf7df
Bheb Developer <bhebdeveloper@gmail.com>
2024-07-19 15:32:00 +0530

    19/07/2024

--- Two Pointers/ContainerWithMostWater.java
+++ Two Pointers/ContainerWithMostWater.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class MaxWaterContainer {
+public class ContainerWithMostWater {
     public static void main(String[] args) {
       int[] heights = {1,8,6,2,5,4,8,3,7};
       System.out.print(maxArea(heights));
