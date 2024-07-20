226ce8e4738ac3773936c2d75367ea28a973b484
Bheb Developer <bhebdeveloper@gmail.com>
2024-07-20 08:36:25 +0530

    20/07/2024

--- Two Pointers/ContainerWithMostWater.java
+++ Two Pointers/ContainerWithMostWater.java
@@ -1,23 +1,24 @@
-import java.util.*;
-
 public class ContainerWithMostWater {
     public static void main(String[] args) {
-      int[] heights = {1,8,6,2,5,4,8,3,7};
-      System.out.print(maxArea(heights));
-  }
-  static int maxArea(int[] height) {
-            int left = 0;
+        int[] heights = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+        System.out.print(maxArea(heights));
+    }
+
+    static int maxArea(int[] height) {
+        int left = 0;
         int right = height.length - 1;
         int maxVol = 0;
 
-        while(left < right){
+        while (left < right) {
             int width = right - left;
             int vol = Math.min(height[left], height[right]) * width;
             maxVol = Math.max(maxVol, vol);
 
-            if(height[left] < height[right]) left++;
-            else right--;
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
         }
         return maxVol;
     }
-  }
+}
